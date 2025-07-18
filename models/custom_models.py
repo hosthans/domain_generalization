df1c26e17b012e8c71ba65cf54be1349fa0474cb
johannes-christian.schuster <johannes-christian.schuster@stud.uni-bamberg.de>
2025-07-18 19:25:49 +0200

    more experiments

--- models/custom_models.py
+++ models/custom_models.py
@@ -112,7 +112,7 @@ def resnet18_fc512_ms1_a0d2(num_classes, loss='softmax', pretrained=True, **kwar
     return model
 
 
-"""ResNet + FC + Freezing"""
+"""ResNet + Freezing"""
 
 
 def resnet18_fr123(num_classes, loss='softmax', pretrained=True, **kwargs):
@@ -133,6 +133,23 @@ def resnet18_fr123(num_classes, loss='softmax', pretrained=True, **kwargs):
 
 
 """ResNet + FC + Freezing"""
+
+
+def resnet50_fc512_fr12(num_classes, loss='softmax', pretrained=True, **kwargs):
+    model = ResNet(
+        num_classes=num_classes,
+        loss=loss,
+        block=Bottleneck,
+        layers=[3, 4, 6, 3],
+        last_stride=1,
+        fc_dims=[512],
+        dropout_p=None,
+        frozen_layers=['layer1', 'layer2'],
+        **kwargs
+    )
+    if pretrained:
+        init_pretrained_weights(model, model_urls['resnet50'])
+    return model
 
 
 def resnet50_fc512_fr123(num_classes, loss='softmax', pretrained=True, **kwargs):
@@ -284,6 +301,25 @@ def resnet50_fc512_ms12_a0d1_fr1(num_classes, loss='softmax', pretrained=True, *
         mixstyle_layers=['layer1', 'layer2'],
         mixstyle_alpha=0.1,
         frozen_layers=['layer1'],
+        **kwargs
+    )
+    if pretrained:
+        init_pretrained_weights(model, model_urls['resnet50'])
+    return model
+
+
+def resnet50_fc512_ms2_a0d1_fr12(num_classes, loss='softmax', pretrained=True, **kwargs):
+    model = ResNet(
+        num_classes=num_classes,
+        loss=loss,
+        block=Bottleneck,
+        layers=[3, 4, 6, 3],
+        last_stride=1,
+        fc_dims=[512],
+        dropout_p=None,
+        mixstyle_layers=['layer2'],
+        mixstyle_alpha=0.1,
+        frozen_layers=['layer1', 'layer2'],
         **kwargs
     )
     if pretrained:
